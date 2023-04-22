51df10dcf9be7a09802c1108d95018f03f9f4cc6
sang <sangfc774@gmail.com>
2023-04-22 14:11:11 +0700

    push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let video = document.querySelector("video");
 console.log(video);
-if (navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices.getUserMedia()) {
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then((s) => {
