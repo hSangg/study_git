789b29e62fd79b35ece13de39f89ebb709da5818
sang <sangfc774@gmail.com>
2023-04-22 14:02:25 +0700

    push

---
+++ index.js
@@ -0,0 +1,12 @@
+let video = document.querySelector("video");
+console.log(video);
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((s) => {
+      video.srcObject = s;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
